635cd36e95fedc98fb806ea2fd2808a1fd454a8f
adcar <alexanderdcardosi@gmail.com>
2020-02-25 14:33:19 -0500

    Add more services

--- src/services.ts
+++ src/services.ts
@@ -1,3 +1,14 @@
-const services = ["Apache", "OpenSSH", "Ubuntu", "Wordpress"];
+const services = [
+  "Apache",
+  "Nginx",
+  "LigHTTPd",
+  "OpenSSH",
+  "Ubuntu",
+  "Debian",
+  "CentOS",
+  "Wordpress",
+  "Joomla",
+  "Drupal"
+];
 
 export default services;
