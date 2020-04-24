c371252cf8520183ee8f7caa47734af917873844
adcar <alexanderdcardosi@gmail.com>
2020-04-24 13:55:47 -0400

    Clean up

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -63,8 +63,6 @@ export default function Search(props: IProps) {
   const { q } = router.query;
   function handleSubmit(e) {
     e.preventDefault();
-    // setOpen(true);
-    // textRef.current.blur();
     if (term !== "") router.push(`/results?q=${term}`);
   }
   return (
